d704e14cf2d2c392dfa90141e3c2728e92d6702e
Leo Belyi <leonid@ac93.org>
2019-02-20 20:58:08 -0500

    Updates

---
+++ precache-manifest.5dde98e050853a36e6e73139ba897ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb2c9f694806d35e91ea",
+    "url": "/leo-react-website/static/css/main.e8c9193b.chunk.css"
+  },
+  {
+    "revision": "cb2c9f694806d35e91ea",
+    "url": "/leo-react-website/static/js/main.cb2c9f69.chunk.js"
+  },
+  {
+    "revision": "0b3edb420f25d98faaf3",
+    "url": "/leo-react-website/static/js/1.0b3edb42.chunk.js"
+  },
+  {
+    "revision": "d207a84391445bfa5b90",
+    "url": "/leo-react-website/static/js/runtime~main.d207a843.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leo-react-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5989bc3fd0628d241e2785aa6be40b20",
+    "url": "/leo-react-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.19f483f8410410ae42217ea2b3e12e0e.js"
+  "/leo-react-website/precache-manifest.5dde98e050853a36e6e73139ba897ec4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/home/index.html", {
+workbox.routing.registerNavigationRoute("/leo-react-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
